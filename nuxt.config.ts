fcf3c4ff3a68228c7aaaf8f259dcc3357a10693e
Alex Smith <alexoesmith@gmail.com>
2025-02-09 13:10:30 +0000

    Project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      title: "Weather App",
+    },
+  },
+
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+});
