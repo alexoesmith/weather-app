e28e08fab6d6ba5107308b67bd1c1a1d29290471
Alex Smith <alexoesmith@gmail.com>
2025-02-09 13:49:53 +0000

    Added type declarations based on selected WeatherAPI response fields

---
+++ types/weather.d.ts
@@ -0,0 +1,25 @@
+export interface WeatherData {
+  error?: {
+    code: number;
+    message: string;
+  };
+  location: {
+    name: string;
+    region: string;
+    country: string;
+    lat: number;
+    lon: number;
+    tz_id: string;
+    localtime_epoch: number;
+    localtime: string;
+  };
+  current: {
+    last_updated_epoch: number;
+    temp_c: number;
+    is_day: number;
+    condition: {
+      text: string;
+      icon: string;
+    };
+  };
+}
