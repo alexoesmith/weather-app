799863d08bb17b796da677ad9260c50ab99daad8
Alex Smith <alexoesmith@gmail.com>
2025-02-09 14:46:29 +0000

    Added AutoAnimate for subtle animation

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { vAutoAnimate } from "@formkit/auto-animate";
+
 const city = ref("");
 const { weatherData, error, loading, fetchWeather } = useWeather();
 </script>
 
 <template>
   <div
+    v-auto-animate
     class="sm:py-20 py-10 max-w-2xl mx-auto px-4 sm:px-8 flex flex-col gap-y-10"
   >
     <AppLogo />
