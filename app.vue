432b5fc94958e2db55f7f4109683d090b8d73ab8
Alex Smith <alexoesmith@gmail.com>
2025-02-09 14:13:29 +0000

    create useWeather composable based on mock API responses

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const city = ref("");
+const { weatherData, error, loading, fetchWeather } = useWeather();
 </script>
 
 <template>
@@ -7,14 +8,15 @@ const city = ref("");
     class="sm:py-20 py-10 max-w-2xl mx-auto px-4 sm:px-8 flex flex-col gap-y-10"
   >
     <AppLogo />
-    <pre>
-      {{ city }}
-    </pre>
     <WeatherSearch v-model="city" />
-    <AppError>Something went wrong. Please try again.</AppError>
-    <LoadingSpinner />
-    <WeatherDisplay />
-    <AppButton>Search Weather</AppButton>
+    <AppError v-if="error">
+      {{ error }}
+    </AppError>
+    <LoadingSpinner v-if="loading" />
+    <WeatherDisplay v-if="weatherData" />
+    <AppButton :disabled="loading">
+      {{ loading ? "Loading..." : "Search Weather" }}
+    </AppButton>
     <AppFooter />
   </div>
 </template>
---
+++ composables/useWeather.ts
@@ -0,0 +1,51 @@
+import type { WeatherData } from "~/types/weather";
+import type { NuxtError } from "#app";
+
+export function useWeather() {
+  const weatherData = useState<WeatherData | null>("weatherData", () => null);
+  const error = ref<string | null>(null);
+  const loading = ref<boolean>(false);
+
+  const fetchWeather = async (city: string) => {
+    if (!city) {
+      error.value = "Please enter a city name";
+      return;
+    }
+
+    if (city === weatherData.value?.location.name) return;
+
+    loading.value = true;
+    error.value = null;
+
+    try {
+      const data = await $fetch<WeatherData>("/api/weather", {
+        method: "GET",
+        query: { city: city },
+      });
+
+      if (data.error) {
+        error.value =
+          data.error?.message ||
+          "Unable to fetch weather data. Please try again later.";
+        return;
+      }
+
+      weatherData.value = data;
+    } catch (e) {
+      if (e instanceof Error) {
+        error.value =
+          (e as NuxtError).statusMessage ||
+          e.message ||
+          "An unexpected error occurred.";
+      } else {
+        error.value = "An unknown error occurred. Please try again later.";
+      }
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  // Ability to create additional functions e.g. searchWeather
+
+  return { weatherData, error, loading, fetchWeather };
+}
